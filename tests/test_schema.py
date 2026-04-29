e6a5a17e1586822c7478b46ece89a0bbf8b2bf54
sjamillah <j.ssozi@alustudent.com>
2026-04-29 12:24:06 +0200

    some little changes in schema

---
+++ tests/test_schema.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_openapi_schema_includes_short_code_and_request_body(client):
+    response = client.get("/api/schema/", HTTP_ACCEPT="application/json")
+
+    assert response.status_code == 200
+
+    schema = response.json()
+
+    create_path = schema["paths"]["/api/urls/"]["post"]
+    redirect_path = schema["paths"]["/{short_code}/"]["get"]
+
+    assert create_path["requestBody"]["content"]["application/json"]["schema"]
+    assert redirect_path["parameters"][0]["name"] == "short_code"
